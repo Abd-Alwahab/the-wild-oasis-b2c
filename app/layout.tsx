084053c50bb94b46b06152b4991682704ad01aa9
AbdAlwahab <abdalwahabb7@gmail.com>
2024-07-09 14:52:05 +0300

    Adding google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@ import Navigation from "./_components/Navigation";
 import { Metadata } from "next";
 import Logo from "./_components/Logo";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: {
@@ -10,13 +16,14 @@ export const metadata: Metadata = {
     default: "Welcome | The Wild Oasis",
   },
   description: "The Wild Oasis is a place to relax and enjoy nature.",
-  
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}
+      >
         <header>
           <Logo />
           <Navigation />
