e5736714d1f1f17859760935fd0bf51674d0a3fa
AbdAlwahab <abdalwahabb7@gmail.com>
2024-07-09 12:44:03 +0300

    Intial commit

---
+++ app/account/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Account() {
+  return <h1>Your Account</h1>;
+}
---
+++ app/cabins/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Cabins() {
+  return <h1>Hello Cabins</h1>;
+}
