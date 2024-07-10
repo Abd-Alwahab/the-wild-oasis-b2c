c158573cc7695a54dff1f898d1e3afb119ebf816
AbdAlwahab <bayanadam10@gmail.com>
2024-07-10 11:20:36 +0300

    Adding the cabins listing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bdmulytkkwfaehbvtpuq.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**", // Match any cabin image path
+      },
+    ],
+  },
+};
 
 export default nextConfig;
