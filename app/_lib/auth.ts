886e44f1f4cc58bbe997d0772f0f8fce1a2fdbf8
AbdAlwahab <bayanadam10@gmail.com>
2024-07-14 11:20:06 +0300

    create a user account on first signin

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 export const {
   auth,
@@ -16,6 +17,28 @@ export const {
   callbacks: {
     authorized({ auth }) {
       return !!auth?.user;
+    },
+    async signIn({ user }) {
+      try {
+        const existingUser = await getGuest(user?.email ?? "");
+
+        if (!existingUser) {
+          await createGuest({
+            email: user?.email ?? "",
+            fullName: user?.name ?? "",
+          });
+        }
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    async session({ session }) {
+      const guest = await getGuest(session.user.email);
+      (session.user as any).guestId = guest.id;
+
+      return session;
     },
   },
 
